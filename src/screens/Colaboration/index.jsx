ef67b92ef3957aa0cb6c9ea35629c7d92bbbff0c
Nikolokoto <59317741+Nikolokoto@users.noreply.github.com>
2021-05-11 22:43:03 -0400

    Colaboration comp

--- src/screens/Colaboration/index.jsx
+++ src/screens/Colaboration/index.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import imgDrink from '../../assets/img/iconosYPatrones/Patrones/Tomatodo.png'
+import { Layout } from '../../components/Layout'
+// import './aboutUs.scss'
 
 const Colaboration = () => {
+    const img = {
+        url: imgDrink,
+        alt: "Bottle pattern",
+        id: 'img-colaboration'
+    }
     return (
-        <div>
-            Colabora
-        </div>
+        <Layout img={img}>
+            <h2>
+                Colabora
+            </h2>
+            <p>
+                Sinfiltros.org es una multiplataforma de educación sexual que está en constante crecimiento, si quieres colaborar, llena el siguiente formulario:
+            </p>
+            <form >
+                <fieldset class="uk-fieldset">
+                    <div className="uk-margin">
+                        <input class="uk-input" type="text" placeholder="Nombre" />
+                    </div>
+                    <div className="uk-margin">
+                        <input class="uk-input" type="email" placeholder="E-mail" />
+                    </div>
+                    <span>
+                        <button className="uk-button uk-button-primary" type="button">Enviar</button>
+                    </span>
+                </fieldset>
+            </form>
+        </Layout>
     )
 }
 
